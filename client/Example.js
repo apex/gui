512f5cf46a0aadd06eb477ac20c270291b2a11d7
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 21:42:54 -0800

    misc

--- client/Example.js
+++ client/Example.js
@@ -6,13 +6,13 @@ import './Example.css'
  * Example is an component example .
  */
 
-export default function({ name, desc, config }) {
+export default function({ name, desc, config, onChange }) {
   return <div class="Example">
     <div class="info">
       <span class="name">{name}</span>
       <p class="desc">{desc}</p>
     </div>
-    <Editor id={name} value={JSON.stringify(config, null, 2)} />
+    <Editor id={name} value={JSON.stringify(config, null, 2)} onChange={onChange} />
     <Preview name={name} config={config} />
   </div>
 }
