d78dd70ec6e4c55ca19a1b8aa7d6f311e447fb44
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 21:28:58 -0800

    editor

---
+++ client/Editor.js
@@ -0,0 +1,22 @@
+import AceEditor from 'react-ace'
+import brace from 'brace'
+import 'brace/mode/json'
+import 'brace/theme/github'
+import './Editor.css'
+
+/**
+ * Editor is a code editor.
+ */
+
+export default function({ id, value }) {
+  return <div class="Editor">
+    <AceEditor
+      name={id}
+      mode="json"
+      theme="github"
+      value={value}
+      onChange={e => console.log(e)}
+      editorProps={{ $blockScrolling: true }}
+    />
+  </div>
+}
--- client/Example.js
+++ client/Example.js
@@ -1,4 +1,5 @@
 import Preview from './Preview'
+import Editor from './Editor'
 import './Example.css'
 
 /**
@@ -9,6 +10,7 @@ export default function({ name, desc, config }) {
   return <div class="Example">
     <span class="name">{name}</span>
     <p class="desc">{desc}</p>
+    <Editor id={name} value={JSON.stringify(config, null, 2)} />
     <Preview name={name} config={config} />
   </div>
 }
