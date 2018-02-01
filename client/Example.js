02a4ee96640033b58aada52b9cd1955ad757cd5a
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 21:54:33 -0800

    state

--- client/Example.js
+++ client/Example.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 import Preview from './Preview'
 import Editor from './Editor'
 import './Example.css'
@@ -6,13 +7,24 @@ import './Example.css'
  * Example is an component example .
  */
 
-export default function({ name, desc, config, onChange }) {
-  return <div class="Example">
-    <div class="info">
-      <span class="name">{name}</span>
-      <p class="desc">{desc}</p>
+export default class extends Component {
+  render() {
+    const { name, desc, config } = this.props
+
+    return <div class="Example">
+      <div class="info">
+        <span class="name">{name}</span>
+        <p class="desc">{desc}</p>
+      </div>
+
+      <Editor
+        id={name}
+        value={JSON.stringify(config, null, 2)}
+        onChange={e => this.onChnage(e)} />
+
+      <Preview
+        name={name}
+        config={config} />
     </div>
-    <Editor id={name} value={JSON.stringify(config, null, 2)} onChange={onChange} />
-    <Preview name={name} config={config} />
-  </div>
+  }
 }
