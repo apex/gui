82e52bbcdca599b5ad9142e77e26a7a975d35e31
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 21:20:39 -0800

    config

--- client/index.js
+++ client/index.js
@@ -4,6 +4,15 @@ import Example from './Example'
 import App from './App'
 
 ReactDOM.render(<App>
-  <Example name="FlatButton" desc="A simple flat button with configurable color." />
-  <Example name="ShadowButton" desc="A stylish shadowed button with configurable color." />
+  <Example
+    name="FlatButton"
+    desc="A simple flat button with configurable color."
+    config={{ text: "SUBSCRIBE", color: '7956EF' }}
+  />
+
+  <Example
+    name="ShadowButton"
+    desc="A stylish shadowed button with configurable color."
+    config={{ text: "SUBSCRIBE", color: '7956EF' }}
+  />
 </App>, document.querySelector('#app'))
