9e1dbc48e76345251203454a5379021d117eceb9
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 19:48:09 -0800

    misc

--- main.go
+++ main.go
@@ -1,13 +1,62 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/apex/gui/components"
+	"github.com/apex/log"
+	"github.com/tj/go/env"
 )
 
 func main() {
-	components.Render(os.Stdout, "ShadowButton", components.Config{
+	addr := ":" + env.GetDefault("PORT", "3000")
+	err := http.ListenAndServe(addr, http.HandlerFunc(serve))
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+}
+
+// serve the request.
+func serve(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/_health" {
+		fmt.Fprintln(w, ":)")
+		return
+	}
+
+	name := "ShadowButton"
+
+	ctx := log.WithFields(log.Fields{
+		"name": name,
+	})
+
+	ctx.Info("rendering")
+
+	var buf bytes.Buffer
+	err := components.Render(&buf, name, components.Config{
 		"text": "SUBSCRIBE",
 	})
+
+	if err != nil {
+		ctx.WithError(err).Error("rendering")
+		http.Error(w, "Error rendering.", http.StatusBadRequest)
+		return
+	}
+
+	setETag(w, buf.Bytes())
+	w.Header().Set("Content-Type", "image/svg+xml")
+	w.Header().Set("Cache-Control", "private")
+	io.Copy(w, &buf)
+}
+
+// setETag sets the etag for body.
+func setETag(w http.ResponseWriter, body []byte) {
+	hash := md5.New()
+	hash.Write(body)
+	etag := hex.EncodeToString(hash.Sum(nil))
+	w.Header().Set("ETag", `w/"`+etag+`"`)
 }
