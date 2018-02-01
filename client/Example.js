01d9f575814e7d7e5b8ac76842f41e3eecf539df
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 21:07:07 -0800

    misc

---
+++ client/Example.js
@@ -0,0 +1,12 @@
+import './Example.css'
+
+/**
+ * Example is an component example .
+ */
+
+export default function({ name, desc }) {
+  return <div class="Example">
+    <span class="name">{name}</span>
+    <p class="desc">{desc}</p>
+  </div>
+}
--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-console.log('test')
+
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.querySelector('#app'))
