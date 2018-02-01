86aac396ae7fb0d20b59f4855e49042958cc922d
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 20:18:22 -0800

    mic

--- main.go
+++ main.go
@@ -15,9 +15,12 @@ import (
 	"github.com/tj/go/http/request"
 
 	"github.com/apex/gui/components"
-	"github.com/apex/gui/views"
 )
 
+// static files.
+var static = http.FileServer(http.Dir("public"))
+
+// main function.
 func main() {
 	addr := ":" + env.GetDefault("PORT", "3000")
 	err := http.ListenAndServe(addr, http.HandlerFunc(handle))
@@ -28,16 +31,18 @@ func main() {
 
 // handle the request.
 func handle(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/_health" {
+	switch r.URL.Path {
+	case "/_health":
 		fmt.Fprintln(w, ":)")
-		return
+	case "/component":
+		component(w, r)
+	default:
+		static.ServeHTTP(w, r)
 	}
+}
 
-	if r.Method == "GET" && r.URL.Path == "/" {
-		index(w, r)
-		return
-	}
-
+// component renders a component.
+func component(w http.ResponseWriter, r *http.Request) {
 	name := request.Param(r, "name")
 	config := request.Param(r, "config")
 	var c components.Config
@@ -68,11 +73,6 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	w.Header().Set("Cache-Control", "private")
 	io.Copy(w, &buf)
-}
-
-// index page.
-func index(w http.ResponseWriter, r *http.Request) {
-	views.Render(w, "index", nil)
 }
 
 // setETag sets the etag for body.
