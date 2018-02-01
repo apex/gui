a6d8e8ce1ba030424ae9607dd9dba16fe303df4f
TJ Holowaychuk <tj@vision-media.ca>
2018-01-31 20:03:55 -0800

    mics

--- components/components.go
+++ components/components.go
@@ -9,13 +9,8 @@ import (
 // defaultFont is a list of system fonts.
 const defaultFont = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`
 
-// helper functions.
-var helpers = template.FuncMap{
-	"default": getDefault,
-}
-
 // tmpls is the components available for rendering.
-var tmpls = template.Must(template.New("").Funcs(helpers).ParseGlob("components/*.svg"))
+var tmpls = template.Must(template.New("").ParseGlob("components/*.svg"))
 
 // Config is the configuration variables for a component.
 type Config map[string]interface{}
@@ -27,12 +22,4 @@ func Render(w io.Writer, name string, c Config) error {
 	}
 
 	return tmpls.ExecuteTemplate(w, name+".svg", c)
-}
-
-// getDefault returns a or b.
-func getDefault(a, b string) string {
-	if a == "" {
-		return b
-	}
-	return a
 }
