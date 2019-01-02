b8b5c52a2ed7e6e21ba350893eece6e506b80b78
catalinmiron <mironcatalin@gmail.com>
2019-01-02 16:21:48 +0100

    Restyle main screen and remove Editor component #5

--- client/Example.js
+++ client/Example.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import debounce from 'lodash.debounce'
 import Preview from './Preview'
-import Editor from './Editor'
+import Input from './Input'
 import './Example.css'
+import { TwitterPicker } from 'react-color';
 
 /**
  * Example is an component example.
@@ -11,25 +12,27 @@ import './Example.css'
  * the preview and update the copy/paste link.
  */
 
+const DEFAULT_COLORS = ['#D9E3F0', '#F47373', '#697689', '#37D67A', '#2CCCE4', '#555555', '#dce775', '#ff8a65', '#ba68c8', '#7956EF'];
+
 export default class extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      config: props.config
+      ...props.config
     }
 
-    this.onChangeDebounced = debounce(this.onChange, 400)
+    this.onChangeDebounced = debounce(this.onChange, 400);
   }
 
-  onChange(config) {
+  onChange(key, value) {
     this.setState({
-      config: JSON.parse(config)
+      [key]: value
     })
   }
 
   render() {
     const { name, desc, count } = this.props
-    const { config } = this.state
+    const { text, color } = this.state
 
     return <div class="Example">
       <div class="info">
@@ -38,14 +41,21 @@ export default class extends Component {
       </div>
 
       <div class="content">
-        <Editor
-          id={name}
-          value={JSON.stringify(config, null, 2)}
-          onChange={e => this.onChangeDebounced(e)} />
-
+        <Input
+          label="Button text:"
+          value={text}
+          placeholder="(E.g: SUBSCRIBE)"
+          onChange={value => this.onChangeDebounced('text', value)}
+        />
+        <TwitterPicker
+          color={color}
+          triangle="hide"
+          colors={DEFAULT_COLORS}
+          onChangeComplete={(color) => this.onChangeDebounced('color', color.hex.substr(1))}
+        />
         <Preview
           name={name}
-          config={config}
+          config={this.state}
           count={count} />
       </div>
     </div>
